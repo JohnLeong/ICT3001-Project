5e5840312c5337847e0d519132bd0ddf8253019c
JohnLeong <johnleongjs@gmail.com>
2021-11-17 15:38:49 +0800

    FIxed bug

--- ict3001/src/components/VideoSection.js
+++ ict3001/src/components/VideoSection.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 export default function VideoSection(props) {
+  const [videoLink, setVideoLink] = useState("");
+
+  useEffect(() => {
+    setVideoLink(props.section.link);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    setVideoLink(props.section.link);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.section]);
+
   return (
     <div>
       <video width="608" height="342" controls className="video">
-        <source src={"/Videos/" + props.section.link} type="video/mp4" />
+        <source src={"/Videos/" + videoLink} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="video-description">
